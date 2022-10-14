48f0fd86d89a65da0152143cd5b776f6ebd03f70
FrostSigil <53145101+FrostSigil@users.noreply.github.com>
2022-10-14 12:59:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -273,10 +273,10 @@ module.exports = function ProxyMenu(mod) {
 		}
 	});
 
-	mod.hook('S_DIALOG', '*', (e) => { // автовход в данж без подтверждения
+	mod.hook('S_DIALOG', '*', (e) => {
 		if (!e.buttons.length) return
 		for (let i = 0; i < e.buttons.length; i++) { 
-		if ([1, 2, 3, 4, 5, 51, 53, 54, 55, 56, 63].includes(e.buttons[i].type)) e.buttons[i].type = 43 }
+		if ([2].includes(e.buttons[i].type)) e.buttons[i].type = 43 }
 		e.type = 1
 		return true
 	});

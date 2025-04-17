0bd3ee9b3d353712bc17a163afe44b3bd78682f3
orynes <a.mocharny@mail.ru>
2025-04-17 23:50:45 +0300

    upd open boxer

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-lonely-if */
 /* eslint-disable no-shadow */
 /* eslint-disable no-undef */
 /* eslint-disable no-param-reassign */
@@ -101,6 +102,7 @@ module.exports = function ProxyMenu(mod) {
 	let contractType = null;
 	let opening = false;
 	let gachaId = null;
+	let gacha = false;
 
 	if (mod.majorPatchVersion >= 94) {
 		// enable padding
@@ -817,6 +819,51 @@ module.exports = function ProxyMenu(mod) {
 		}
 	});
 
+	mod.hook("C_USE_ITEM", 3, event => {
+		if (!mod.settings.openbox) return;
+		if (!opening) {
+			gachaId = event.id;
+			mod.hookOnce("S_REQUEST_CONTRACT", "*", (e) => {
+				if ([33, 44, 53].includes(e.type)) {
+					opening = true;
+					openGacha(gachaId);
+					if (mod.game.inventory.getTotalAmount(gachaId) >= 5) {
+						mod.command.message("Открываю. Для остановки кликните еще раз на предмет.");
+					}
+				}
+			});
+		} else {
+			opening = false;
+			openGacha(gachaId);
+			gachaId = null;
+			mod.command.message("Остановка.");
+			return false;
+		}
+	});
+
+	mod.hook("S_GACHA_START", 2, () => {
+		if (!mod.settings.openbox) return;
+		if (opening && gachaId !== null) {
+			gacha = true;
+			openGacha(gachaId);
+		}
+		return false;
+	});
+
+	mod.hook("S_RESULT_EVENT_SEED", 2, () => {
+		if (!mod.settings.openbox) return;
+		if (opening && gachaId !== null) {
+			openGacha(gachaId);
+		}
+	});
+
+	mod.hook("S_MERGE_ITEM_EXECUTE_RESULT", "raw", () => {
+		if (!mod.settings.openbox) return;
+		if (opening && gachaId !== null) {
+			mod.setTimeout(() => openGacha(gachaId), mod.settings.boxdelay + 1000);
+		}
+	});
+
 	// /////////////////////
 	// //// FUNCTION ///////
 	// /////////////////////
@@ -840,29 +887,37 @@ module.exports = function ProxyMenu(mod) {
 
 	function openGacha(id) {
 		if (opening && mod.game.inventory.getTotalAmount(id) >= 1) {
-			mod.send("C_GACHA_TRY", mod.majorPatchVersion <= 93 ? 1 : 2, { id: contract, amount: 1 });
-			mod.hookOnce("S_GACHA_END", mod.majorPatchVersion <= 93 ? 1 : "*", () => {
-				mod.setTimeout(() => (mod.majorPatchVersion <= 93 ?
-					mod.send("C_USE_ITEM", 3, {
-						gameId: mod.game.me.gameId,
-						id: id,
-						amount: 1,
-						loc: player.loc,
-						w: player.loc.w,
-						unk4: true
-					}) :
-					openGacha(id)), mod.settings.boxdelay);
-				return false;
-			});
+			if (gacha) {
+				mod.send("C_GACHA_TRY", mod.majorPatchVersion <= 93 ? 1 : 2, { id: contract, amount: 1 });
+				mod.hookOnce("S_GACHA_END", mod.majorPatchVersion <= 93 ? 1 : "*", () => {
+					mod.setTimeout(() => (mod.majorPatchVersion <= 93 ?
+						useItem(id) :
+						openGacha(id)), mod.settings.boxdelay);
+					return false;
+				});
+			} else {
+				mod.setTimeout(() => useItem(id), mod.settings.boxdelay);
+			}
 		} else {
-			mod.send("C_GACHA_CANCEL", "*", { id: contract });
-			mod.send("C_CANCEL_CONTRACT", "*", { type: contractType, id: contract });
-			mod.hookOnce("S_CANCEL_CONTRACT", "*", () => {
+			if (gacha) {
+				if (mod.majorPatchVersion >= 93) {
+					mod.send("C_GACHA_CANCEL", 1, { id: contract });
+				}
+				mod.send("C_CANCEL_CONTRACT", 1, { type: contractType, id: contract });
+				mod.hookOnce("S_CANCEL_CONTRACT", 1, () => {
+					opening = false; gacha = false;
+					if (mod.game.inventory.getTotalAmount(id) < 1) {
+						gachaId = null;
+						mod.command.message("Закончил открытие.");
+					}
+				});
+			} else {
 				opening = false;
 				if (mod.game.inventory.getTotalAmount(id) < 1) {
+					gachaId = null;
 					mod.command.message("Закончил открытие.");
 				}
-			});
+			}
 		}
 	}
 
